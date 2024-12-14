cc183796f6db1df904c2acbfd26fa5bdcc7949b8
Hans Liss <Hans@Liss.pp.se>
2024-12-14 06:10:31 +0000

    Fixed some datatype and pthread bugs and cleaned up logging.

--- thmand.c
+++ thmand.c
@@ -22,6 +22,9 @@
 #include <time.h>
 #include <sys/mman.h>
 
+#undef LOGGING
+#undef DEBUG
+
 FILE *logfile;
 
 #define FLOAT long double
@@ -40,23 +43,24 @@ FILE *logfile;
 #define THMAND_MODE_CONTINUE 1
 #define THMAND_MODE_UPDATE 2
 
-unsigned long width=1024;
-unsigned long height=1024;
-unsigned long maxiter=1048576L;
+int32_t width=1024;
+int32_t height=1024;
+int32_t maxiter=1048576L;
 char logprefix[128];
 
-static int *dumpbuffer=NULL;
-static long totpix_done=0;
-static long lasttotpix_done=0;
+static uint32_t *dumpbuffer=NULL;
+static int32_t totpix_done=0;
+static int32_t lasttotpix_done=0;
+static double lastTotpixPercent=0;
 
 static FLOAT minde=1E14;
 static FLOAT maxde=-1E14;
 
 typedef struct fileheader_s {
-  int width;
-  int height;
-  int maxiter;
-  int reserved;
+  uint32_t width;
+  uint32_t height;
+  uint32_t maxiter;
+  uint32_t reserved;
 } fileheader;
 
 void ld_epsilon(long double *rval) {
@@ -118,14 +122,14 @@ char *make_message(const char *fmt, va_list *ap) {
 
 
 void debug(const char *fmt, ...) {
-  /*
+#ifdef DEBUG
     va_list ap;
     va_start(ap, fmt);
     char *msg = make_message(fmt, &ap);
     va_end(ap);
     fprintf(stderr, "%s\n", msg);
     free(msg);
-  */
+#endif
 }
 
 struct taskconfig {
@@ -134,11 +138,12 @@ struct taskconfig {
   unsigned int y0;
   unsigned int x1;
   unsigned int y1;
-  unsigned long maxiter;
+  int32_t maxiter;
   FLOAT de_k;
   int de_logr;
   int mode;
   int done;
+  int terminated;
   pthread_mutex_t *lock;
 };
 
@@ -184,8 +189,8 @@ workitem popwork(workitem *stack) {
   return tmp;
 }
 
-unsigned long mandel(FLOAT cx, FLOAT cy, unsigned long maxiter) {
-  unsigned long i=maxiter;
+int32_t mandel(FLOAT cx, FLOAT cy, int32_t maxiter) {
+  int32_t i=maxiter;
   FLOAT zx=cx,zy=cy,zx2=cx*cx,zy2=cy*cy;
   while((i>0)&&(zx2+zy2 < 4.0)) {
     zy=2*zx*zy + cy;
@@ -195,13 +200,13 @@ unsigned long mandel(FLOAT cx, FLOAT cy, unsigned long maxiter) {
     i--;
   }
 #ifdef DEBUG
-  fprintf(logfile,"mandel=%u, maxiter=%u\n",i,maxiter);
+  fprintf(logfile,"mandel=%u, maxiter=%Lu\n",i,maxiter);
 #endif
   return maxiter-i;
 }
  
-unsigned long mandelspeed(FLOAT cx, FLOAT cy, unsigned long maxiter) {
-  unsigned long i=maxiter;
+int32_t mandelspeed(FLOAT cx, FLOAT cy, int32_t maxiter) {
+  int32_t i=maxiter;
   FLOAT zx=cx,zy=cy,zx2=cx*cx,zy2=cy*cy;
   long double ox=cx,oy=cy;
   while((i>0)&&(zx2+zy2 < 4.0)) {
@@ -213,13 +218,13 @@ unsigned long mandelspeed(FLOAT cx, FLOAT cy, unsigned long maxiter) {
     i--;
   }
 #ifdef DEBUG
-  fprintf(logfile,"mandel=%u, maxiter=%u\n",i,maxiter);
+  fprintf(logfile,"mandel=%u, maxiter=%Lu\n",i,maxiter);
 #endif
-  return (unsigned long)((long double)maxiter * sqrt((zx-ox)*(zx-ox)+(zy-oy)*(zy-oy)) / SPEED_MAX);
+  return (int32_t)((long double)maxiter * sqrt((zx-ox)*(zx-ox)+(zy-oy)*(zy-oy)) / SPEED_MAX);
 }
  
-unsigned long mandelangle(FLOAT cx, FLOAT cy, unsigned long maxiter) {
-  unsigned long i=maxiter;
+int32_t mandelangle(FLOAT cx, FLOAT cy, int32_t maxiter) {
+  int32_t i=maxiter;
   FLOAT zx=cx,zy=cy,zx2=cx*cx,zy2=cy*cy;
   long double ox=cx,oy=cy;
   while((i>0)&&(zx2+zy2 < 4.0)) {
@@ -233,7 +238,7 @@ unsigned long mandelangle(FLOAT cx, FLOAT cy, unsigned long maxiter) {
 #ifdef DEBUG
   fprintf(logfile,"mandel=%u, maxiter=%u\n",i,maxiter);
 #endif
-  return (unsigned long)(maxiter*(3.1415926535+atan2((zx-ox),(zy-oy)))/6.28320);
+  return (int32_t)(maxiter*(3.1415926535+atan2((zx-ox),(zy-oy)))/6.28320);
 }
 
 /*
@@ -243,8 +248,8 @@ unsigned long mandelangle(FLOAT cx, FLOAT cy, unsigned long maxiter) {
     2 * (zx * dzx + zx * dzy * i + zy * dzx * i - zy * dzy) + 1
     2 * zx * dzx - 2 * zy * dzy + (2 * zx * dzy + 2* zy * dzx) * i + 1
 */ 
-unsigned long mandelde(FLOAT cx, FLOAT cy, unsigned long maxiter, FLOAT k, int logr) {
-  unsigned long i=maxiter;
+int32_t mandelde(FLOAT cx, FLOAT cy, int32_t maxiter, FLOAT k, int logr) {
+  int32_t i=maxiter;
   FLOAT zx=cx,zy=cy,zx2=cx*cx,zy2=cy*cy;
   FLOAT dzx = 0, dzy = 0;
   while((i>0)&&(zx2+zy2 <= 1E6)) {
@@ -273,7 +278,7 @@ unsigned long mandelde(FLOAT cx, FLOAT cy, unsigned long maxiter, FLOAT k, int l
       r = ((FLOAT)maxiter) * de / k;
     }
     if (r < 0) r=0;
-    return ((long)r) % maxiter;
+    return ((int32_t)r) % maxiter;
   }
 }
  
@@ -284,8 +289,12 @@ void *run_checkfilled(void *cfgi) {
   int x0, x1, y0, y1;
   int changedpixels=0;
   struct taskconfig *cfg = (struct taskconfig *)cfgi;
+  debug("Entering run_checkfilled");
 
-  if (cfg->mode != MODE_NORMAL && cfg->mode != MODE_DE) return NULL;
+  if (cfg->mode != MODE_NORMAL && cfg->mode != MODE_DE) {
+    cfg->terminated=1;
+    return NULL;
+  }
 
   x0 = cfg->x0;
   x1 = cfg->x1;
@@ -293,6 +302,7 @@ void *run_checkfilled(void *cfgi) {
   y1 = cfg->y1;
   yval=YVAL(y0);
   yval1=YVAL(y1);
+  debug("Starting loop");
   for (x = x0; x<= x1; x++) {
     int v1 = ntohl(dumpbuffer[4 + x + y0 * width]);
     int v2 = ntohl(dumpbuffer[4 + x + y1 * width]);
@@ -326,6 +336,8 @@ void *run_checkfilled(void *cfgi) {
       break;
     }
   }
+  debug("Finished loop, filled=%d", filled);
+
   if (filled) {
     xval=XVAL(x0);
     xval1=XVAL(x1);
@@ -376,9 +388,12 @@ void *run_checkfilled(void *cfgi) {
       totpix_done += changedpixels;
       pthread_mutex_unlock(cfg->lock);
       cfg->done = 1;
+      cfg->terminated = 1;
       return NULL;
     }
   }
+  debug("Returning");
+  cfg->terminated = 1;
   return NULL;
 }
 
@@ -427,6 +442,7 @@ void *run_dowork(void *cfgi) {
   pthread_mutex_unlock(cfg->lock);
   cfg->done=1;
 
+  cfg->terminated = 1;
   return NULL;
 }
 
@@ -439,7 +455,9 @@ void usage(char *progname) {
 void quadsplit(workitem *stack, workitem current) {
   int xm=(current->x0 + current->x1) / 2;
   int ym=(current->y0 + current->y1) / 2;
+#ifdef LOGGING
   fprintf(stderr, "%sSplitting (%d,%d) - (%d,%d) into four parts\n", logprefix, current->x0, current->y0, current->x1, current->y1);
+#endif
   pushwork(stack, xm + 1, ym + 1, current->x1, current->y1, 0);
   pushwork(stack, current->x0, ym + 1, xm, current->y1, 0);
   pushwork(stack, xm + 1, current->y0, current->x1, ym, 0);
@@ -454,7 +472,7 @@ int main(int argc,char *argv[]) {
   fileheader header;
 
   int tempindex, r, nthreads=0, ncores=1, x, y, tmpval;
-  unsigned long oldmaxiter=0;
+  int32_t oldmaxiter=0;
 
   int o;
 
@@ -462,13 +480,13 @@ int main(int argc,char *argv[]) {
   FILE *deffile, *dumpfile;
   struct stat fstatbuf;
 
-  long wtmp, htmp;
+  int32_t wtmp, htmp;
 
   mpf_t gcentx, gcenty, gdx;
  
   static workitem workstack=NULL, current;
 
-  unsigned long filesize=0;
+  int32_t filesize=0;
   int file_handling=THMAND_MODE_INITIALIZE;
 
   FLOAT de_k=21.5;
@@ -476,7 +494,7 @@ int main(int argc,char *argv[]) {
 
   static pthread_mutex_t lock;
 
-  long totpix=(long)width*height;
+  int32_t totpix=(int32_t)width*height;
 
   long double epsilon;
   ld_epsilon(&epsilon);
@@ -490,8 +508,8 @@ int main(int argc,char *argv[]) {
     case 'p': ncores=atoi(optarg); break;
     case 'w': width=atoi(optarg); break;
     case 'h': height=atoi(optarg); break;
-    case 'm': maxiter=atoi(optarg); break;
-    case 'O': oldmaxiter=atoi(optarg); break;
+    case 'm': maxiter=atol(optarg); break;
+    case 'O': oldmaxiter=atol(optarg); break;
     case 'K': sscanf(optarg, "%Lf", &de_k); break;
     case 'L': de_logr=1; break;
     case 'M':
@@ -512,7 +530,7 @@ int main(int argc,char *argv[]) {
     return -1;
   }
 
-  totpix=(long)width*height;
+  totpix=(int32_t)width*height;
 
   cfg=(struct taskconfig *)malloc(sizeof(struct taskconfig) * ncores);
   threads=(pthread_t *)malloc(sizeof(pthread_t) * ncores);
@@ -558,7 +576,7 @@ int main(int argc,char *argv[]) {
     wtmp=ntohl(header.width);
     htmp=ntohl(header.height);
     if (wtmp != width || htmp != height) {
-      fprintf(stderr, "Existing file has different width/height (%ld,%ld). Aborting.\n", wtmp, htmp);
+      fprintf(stderr, "Existing file has different width/height (%d,%d). Aborting.\n", wtmp, htmp);
       return -3;
     }
     if (filesize < sizeof(header) + sizeof(int)*(width*height)) {
@@ -586,7 +604,7 @@ int main(int argc,char *argv[]) {
     file_handling=THMAND_MODE_INITIALIZE;
   }
 
-  if ((dumpbuffer=(int *)mmap(NULL,
+  if ((dumpbuffer=(uint32_t *)mmap(NULL,
 			      sizeof(header) + 
 			      sizeof(int) * (width * height),
 			       PROT_READ|PROT_WRITE,
@@ -601,7 +619,7 @@ int main(int argc,char *argv[]) {
     fprintf(stderr, "Initializing file...\n");
     // Someone (me) made a mistake early on here, so the width and height
     // are 64-bit values but are treated as 32-bit values in the dump file.
-    // This is why all address uses an offset of 4 ints (2 longs) instead of 2.
+    // This is why all address uses an offset of 4 ints (2 int32_ts) instead of 2.
     dumpbuffer[0]=htonl(width);
     dumpbuffer[1]=htonl(height);
     dumpbuffer[2]=htonl(maxiter);
@@ -611,9 +629,9 @@ int main(int argc,char *argv[]) {
     for (y=0; y<height; y++) {
       fprintf(stderr, "%d\r", y);
       for (x=0; x<width; x++) {
-	dumpbuffer[4 + x + y * width] = htonl((long)-1);
+	dumpbuffer[4 + x + y * width] = htonl((int32_t)-1);
       }
-      msync(&(dumpbuffer[4 + y*width]), width * sizeof(int), MS_ASYNC);
+      msync(&(dumpbuffer[4 + y*width]), width * sizeof(uint32_t), MS_ASYNC);
     }
     fprintf(stderr, "Done.    \n");
     break;
@@ -663,7 +681,7 @@ int main(int argc,char *argv[]) {
   fprintf(stderr, "Current center is %20.20Lf, %20.20Lf, dx=%20.20Lf\n",centx,centy,dx);
   fprintf(stderr, "Corners are %20.20Lf, %20.20Lf  and  %20.20Lf, %20.20Lf\n",
 	  point_x0,point_y0,point_x1,point_y1);
-  fprintf(stderr, "Maxiter is %ld\n", maxiter);
+  fprintf(stderr, "Maxiter is %d\n", maxiter);
 
   pushwork(&workstack, 0, 0, width-1, height-1, 0);
 
@@ -688,10 +706,13 @@ int main(int argc,char *argv[]) {
 	cfg[tempindex].de_k = de_k;
 	cfg[tempindex].de_logr = de_logr;
 	cfg[tempindex].lock = &lock;
+	cfg[tempindex].terminated = 0;
 	if (!pthread_create(&(threads[tempindex]), NULL, run_checkfilled, (void*)&(cfg[tempindex]))) {
 	  nthreads++;
+#ifdef LOGGING
 	  fprintf(stderr, "%sStarted run_checkfilled (thread %d) for (%d;%d),(%d;%d)     \n", logprefix, tempindex,
 		  current->x0, current->y0, current->x1, current->y1);
+#endif
 	} else perror("pthread_create()");
       } else if ((1 + current->x1 - current->x0) > MAX_RENDER_SIZE &&
 		 (1 + current->y1 - current->y0) > MAX_RENDER_SIZE) {
@@ -710,19 +731,27 @@ int main(int argc,char *argv[]) {
 	cfg[tempindex].de_k = de_k;
 	cfg[tempindex].de_logr = de_logr;
 	cfg[tempindex].lock = &lock;
+	cfg[tempindex].terminated = 0;
 	if (!pthread_create(&(threads[tempindex]), NULL, run_dowork, (void*)&(cfg[tempindex]))) {
 	  nthreads++;
+#ifdef LOGGING
 	  fprintf(stderr, "%sStarted run_dowork (thread %d) for (%d;%d),(%d;%d)     \n", logprefix, tempindex, 
 		  current->x0, current->y0, current->x1, current->y1);
+#endif
 	} else perror("pthread_create()");
       }
       free(current);
     }
 
     for (tempindex = 0; tempindex < ncores; tempindex++) {
-      if (cfg[tempindex].taskid >= 0 && pthread_kill(threads[tempindex], 0) != 0) {
+      if (cfg[tempindex].taskid >=0) {
+	//debug("Checking thread %d", tempindex);
+      }
+      if (cfg[tempindex].taskid >= 0 && cfg[tempindex].terminated) {
 	pthread_join(threads[tempindex], (void **)&r);
+#ifdef LOGGING
 	fprintf(stderr, "%sThread %d terminated                          \n", logprefix, tempindex);
+#endif
 	cfg[tempindex].taskid = -1;
 	nthreads--;
 	if (!(cfg[tempindex].done)) {
@@ -731,15 +760,22 @@ int main(int argc,char *argv[]) {
       }
     }
 
-    if (totpix_done != lasttotpix_done) {
-      snprintf(logprefix, sizeof(logprefix), "thmand: %g%% done: ", 100 * (double)totpix_done / (double)totpix);
+    if (totpix_done > lasttotpix_done) {
+      double totpixPercent =  100 * (double)totpix_done / (double)totpix;
+      snprintf(logprefix, sizeof(logprefix), "thmand: %g%% done (%d running threads): ", totpixPercent, nthreads);
+#ifndef LOGGING
+      if (totpixPercent - lastTotpixPercent >= 1) {
+	fprintf(stderr, "%d%% done      \r", (int)totpixPercent);
+	lastTotpixPercent = totpixPercent;
+#endif
+      }
       lasttotpix_done=totpix_done;
     }
 
     if (nthreads == ncores || !workstack) usleep(200);
   }
 
-  if (munmap(dumpbuffer, sizeof(header) + sizeof(int) * (width * height)) == -1) {
+  if (munmap(dumpbuffer, sizeof(header) + sizeof(uint32_t) * (width * height)) == -1) {
     perror("munmap()");
   }
   if (mode==MODE_DE) printf("minde=%Lg, maxde=%Lg\n", minde, maxde);
